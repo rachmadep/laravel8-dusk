8657b4e207e400efae4d7924ee5f0e09e094f934
Rachmad Pribadi <rachmad.eko@mamiteam.com>
2022-01-19 21:21:15 +0700

    add cancel booking test

--- tests/Browser/TenantBookingTest.php
+++ tests/Browser/TenantBookingTest.php
@@ -13,6 +13,8 @@ class TenantBookingTest extends DuskChromeTestCase
     /**
     * @test
     * @group loginTenant
+    * @group tenantBooking
+    * @group tenantCancleBooking
     */
     public function testLoginTenant()
     {
@@ -69,25 +71,59 @@ class TenantBookingTest extends DuskChromeTestCase
                         ->waitUntilMissing('div.swal2-container')
                         ->click('.track_next_booking');
                 })
+                ->waitFor('.booking-summary__tnc')
                 ->assertVisible('.booking-summary__tnc')
                 
-                // ->with('.booking-summary__tnc', function($card) {
-                //     $card->assertVisible('.bg-c-checkbox');
-                // })
+                ->with('.booking-summary__tnc', function($card) {
+                    $card->assertVisible('.bg-c-checkbox');
+                })
 
                 // Check TnC
                 ->with('.bg-c-checkbox', function($card) {
-                    $card->click('.bg-c-checkbox__icon')
-                    ->pause('1000');
+                    $card->click('.bg-c-checkbox__icon');
                 })
 
                 // Click Ajukan Sewa
                 ->with('.booking-summary__booking-action', function($card) {
                     $card->waitFor('.bg-c-button')
-                    ->assertVisible('.bg-c-button')
-                    ->press('.bg-c-button');
+                    ->assertVisible('.bg-c-button');
+                    $card->press('.bg-c-button')->pause('3000');
                 })
                 ;
+        });
+    }
+
+    /**
+    * @test
+    * @group tenantCancleBooking
+    */
+    public function testTenantCancleBookingKost()
+    {
+        $this->browse(function ($browser) {
+            $browser->visit('/user/booking')
+                ->waitFor('.profile-container')
+                ->assertVisible('.profile-container')
+                ->assertVisible('.user-booking-list')
+                ->waitFor('.booking-list-card')
+                ->assertVisible('.booking-list-card')
+                ->with('.booking-list-card', function($card) {
+                    $card->assertSee('Tunggu Konfirmasi');
+                    $card->assertSee('Kos Duwa Tipe 1 Tegalrejo Yogyakarta');
+                    $card->click('.hidden-xs')
+                    ->assertVisible('.detail-cancel')
+                    ->with('.detail-cancel', function($cardCancel) {
+                        $cardCancel->waitFor('.btn-success')
+                            ->scrollIntoView('.btn-success')
+                            ->pause('300')
+                            ->click('.btn-success');
+                    });
+                })
+                ->pause('2000')
+                ->assertSee('Berikan alasan pembatalan booking terhadap pemilik kost')
+                ->assertSee('Ya, Batalkan')
+                ->press('Ya, Batalkan')
+                ->waitForText('Booking Kamu Dibatalkan')
+                ->pause('2000');
         });
     }
 }
